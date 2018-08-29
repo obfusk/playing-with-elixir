53b1c50abbdab54c85a9100f91f86063c93bda80
Felix C. Stegerman <flx@obfusk.net>
2018-08-29 14:44:49 +0200

    use case instead of fn

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -13,10 +13,11 @@ defmodule FizzBuzz do
 
   """
   def fizzbuzz n do
-    (fn 0, 0, _ -> "FizzBuzz"
-        0, _, _ -> "Fizz"
-        _, 0, _ -> "Buzz"
-        _, _, _ -> n
-    end).(rem(n, 3), rem(n, 5), n)
+    case { rem(n, 3), rem(n, 5), n } do
+      { 0, 0, _ } -> "FizzBuzz"
+      { 0, _, _ } -> "Fizz"
+      { _, 0, _ } -> "Buzz"
+      _           -> n
+    end
   end
 end
