5fb99316abaf45f41831a3f630d1870d4bc6ae89
Felix C. Stegerman <flx@obfusk.net>
2018-08-30 03:41:50 +0200

    remove redundant parameter in case

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -13,11 +13,11 @@ defmodule FizzBuzz do
 
   """
   def fizzbuzz n do
-    case { rem(n, 3), rem(n, 5), n } do
-      { 0, 0, _ } -> "FizzBuzz"
-      { 0, _, _ } -> "Fizz"
-      { _, 0, _ } -> "Buzz"
-      _           -> n
+    case { rem(n, 3), rem(n, 5) } do
+      { 0, 0 } -> "FizzBuzz"
+      { 0, _ } -> "Fizz"
+      { _, 0 } -> "Buzz"
+      _        -> n
     end
   end
 end
